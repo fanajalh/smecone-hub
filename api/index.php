2979b9afea97380e7fd4d5c29948d59b42f8deae
fanajalh <muhammadfachriarfan7@gmail.com>
2026-07-18 17:10:42 +0700

    Fix Vercel storage read-only error

--- api/index.php
+++ api/index.php
@@ -1,8 +1,27 @@
 <?php
 
-/**
- * Vercel Serverless Entry Point
- * Meneruskan semua request ke index.php utama Laravel di folder public/
- */
+use Illuminate\Http\Request;
 
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Khusus Vercel Serverless: Direktori root adalah read-only.
+// Kita harus memindahkan seluruh cache dan storage ke /tmp
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+    mkdir($storagePath . '/app', 0777, true);
+    mkdir($storagePath . '/framework/cache/data', 0777, true);
+    mkdir($storagePath . '/framework/views', 0777, true);
+    mkdir($storagePath . '/framework/sessions', 0777, true);
+    mkdir($storagePath . '/logs', 0777, true);
+}
+
+$app->useStoragePath($storagePath);
+
+$app->handleRequest(Request::capture());
